408bb12afe47819332bb43a213d7071ab87b72a1
michaelsatumba <mjsatumba@gmail.com>
2021-10-19 13:58:30 -0700

    remove create event

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-
+var ejs = require("ejs");
 
 var app = express();
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 mongoose.connect("mongodb://localhost:27017/LifterDB", {useNewUrlParser: true});
 
-
-
-
+app.get("/", function(req, res){
+  res.render("../public/html/index")
+})
 
 
 let port = process.env.PORT;
