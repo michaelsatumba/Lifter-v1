534c121cd8c4a3ada502f74811be648c2c2f68e2
michaelsatumba <mjsatumba@gmail.com>
2021-10-19 14:32:11 -0700

    update tnc

--- app.js
+++ app.js
@@ -10,10 +10,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-mongoose.connect("mongodb://localhost:27017/LifterDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
   res.render("../public/html/index")
+})
+
+app.get("/register", function(req, res){
+  res.render("../public/html/register")
+})
+
+app.get("/tnc", function(req, res){
+  res.render("../public/html/tnc")
 })
 
 
