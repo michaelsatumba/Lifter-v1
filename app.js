46da34cd77539110d5e054195dcce4c03e1bfc38
michaelsatumba <mjsatumba@gmail.com>
2021-10-19 12:43:43 -0700

    remove bin - update app.js port

--- app.js
+++ app.js
@@ -37,4 +37,10 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-module.exports = app;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started on port 3000");
+});
